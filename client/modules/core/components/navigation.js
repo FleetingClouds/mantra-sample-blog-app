3dcd071630aeafe8294cf4df8a5a00cd18ae2853
Martin Bramwell <martinhbramwell@gmail.com>
2016-06-09 11:43:26 -0400

    switch from kadira:flow-router to kadira:flow-router-ssr

--- client/modules/core/components/navigation.js
+++ client/modules/core/components/navigation.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Navigation = () => (
   <div>
     <b> Navigation: </b>
-    <a href="/">Home</a> |
-    <a href="/new-post">New Post</a>
+    <a href="/">Home</a> | <a href="/new-post">New Post</a>
   </div>
 );
 
